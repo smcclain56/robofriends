445a0d33bf946bff47cccbbbf411248e271e4f62
Sarah McClain <43018457+smcclain56@users.noreply.github.com>
2020-07-23 14:51:09 -0500

    add a search bar to app and create a state to change

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+const state = {
+    robots: robots,
+    searchfield: ''
+};
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange(event){
+        console.log(event.target.value);
+    } 
+
+    render(){
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={this.state.robots}/>
+            </div>
+        );
+    }
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import Card from './Card';
-import CardList from './CardList';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots'
 
 ReactDOM.render(
-    <CardList robots={robots}/>,
+    <App />,
   document.getElementById('root')
 );
 
